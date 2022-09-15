231d81faa8ce16f7c5d506613b58a537ab0219fe
Paras Rojiya <parasrojiya51@gmail.com>
2022-09-15 11:46:07 +0530

    First Commit

---
+++ lib/globals/global.dart
@@ -0,0 +1,48 @@
+class Global {
+  static List<Map> allContactDetails = <Map>[
+    {
+      'name': 'Darsh',
+      'description': 'Okay',
+      'time': '9:33 am',
+      'image': 'assets/images/darsh.jpg',
+    },
+    {
+      'name': 'Krushang Sutex',
+      'description': 'Moklu Hamna',
+      'time': '9:26 am',
+      'image': 'assets/images/darsh.jpg',
+    },
+    {
+      'name': 'Vivek Tejani RnW',
+      'description': 'lib.zip',
+      'time': 'Yesterday',
+      'image': 'assets/images/darsh.jpg',
+    },
+    {
+      'name': 'Dhaval',
+      'description': '✔✔ 5 min ma avu',
+      'time': 'Yesterday',
+      'image': 'assets/images/darsh.jpg',
+    },
+    {
+      'name': 'Mayur',
+      'description': 'Hi',
+      'time': '30/08/2022',
+      'image': 'assets/images/darsh.jpg',
+    },
+    {
+      'name': 'Vikram',
+      'description': '✔✔ You:2 Vagya sudhi ma...',
+      'time': '10:54 am',
+      'image': 'assets/images/darsh.jpg',
+    },
+    {
+      'name': 'Sahil Khunt Sutex',
+      'description': 'MinorProjectCertificate315.jpg',
+      'time': '4:23 pm',
+      'image': 'assets/images/darsh.jpg',
+    },
+  ];
+
+  static bool isIOS = false;
+}
